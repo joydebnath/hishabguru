4a91600f58477c4f581d2da6725acf69a36f96df
Joy Debnath <jd@genisys.com.au>
2020-10-24 01:43:55 +1100

    icons

--- resources/js/components/global/navbar/menus/InventoryMenu.vue
+++ resources/js/components/global/navbar/menus/InventoryMenu.vue
@@ -11,25 +11,41 @@
         </span>
         <router-link to="/@/products">
             <b-dropdown-item class="py-2 focus:outline-none" aria-role="listitem">
-                Products
+                <div class="flex flex-row">
+                    <Timeline custom_class="h-5 w-5 text-indigo-600" />
+                    <div class="flex flex-col pl-3">
+                        <p class="text-sm leading-6 font-medium text-gray-900">Products</p>
+                        <p class="text-xs leading-5 text-gray-500">List of all of your products and service</p>
+                    </div>
+                </div>
             </b-dropdown-item>
         </router-link>
         <router-link to="/@/product-categories">
             <b-dropdown-item class="py-2 focus:outline-none" aria-role="listitem">
-                Product Categories
+                <div class="flex flex-row">
+                    <Shopping custom_class="h-5 w-5 text-indigo-600" />
+                    <div class="flex flex-col pl-3">
+                        <p class="text-sm leading-6 font-medium text-gray-900">Product Categories</p>
+                        <p class="text-xs leading-5 text-gray-500">List of your categories to classify products</p>
+                    </div>
+                </div>
             </b-dropdown-item>
         </router-link>
-<!--        <router-link to="/@/inventory-sites">-->
-<!--            <b-dropdown-item class="py-2 focus:outline-none" aria-role="listitem">-->
-<!--                Inventory Sites-->
-<!--            </b-dropdown-item>-->
-<!--        </router-link>-->
+        <!--        <router-link to="/@/inventory-sites">-->
+        <!--            <b-dropdown-item class="py-2 focus:outline-none" aria-role="listitem">-->
+        <!--                Inventory Sites-->
+        <!--            </b-dropdown-item>-->
+        <!--        </router-link>-->
     </b-dropdown>
 </template>
 
 <script>
+import Timeline from "@/components/icons/Timeline";
+import Shopping from "@/components/icons/Shopping";
+
 export default {
     name: "InventoryMenu",
+    components: {Shopping, Timeline},
     props: {
         active: String
     },
