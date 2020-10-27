700d7d4ab663b50408d683b329a8246f01d41109
Joy Debnath <jd@genisys.com.au>
2020-10-28 09:20:55 +1100

    chart

---
+++ resources/js/pages/dashboard/widgets/MonthCashFlow.vue
@@ -0,0 +1,72 @@
+<template>
+    <vue-frappe
+        class="bg-white pt-4 shadow frappe"
+        type="axis-mixed"
+        id="one"
+        :labels="labels"
+        :title="title"
+        :height="300"
+        :colors="colors"
+        :dataSets="chartData"
+        :tooltip-options="tooltipOptions"
+        :axis-options="axisOptions"
+        :bar-options="barOptions"
+    >
+    </vue-frappe>
+</template>
+
+<script>
+
+import {VueFrappe} from 'vue2-frappe'
+
+export default {
+    name: "MonthCashFlow",
+    components: {VueFrappe},
+    data() {
+        return {
+            labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            colors: ['#41b7fc', '#e2e8f0'],
+            chartData: [{
+                name: "Cash In", chartType: 'bar',
+                values: [2500, 4000, 3000, 3500, 800, 5200]
+            },
+                {
+                    name: "Cash Out", chartType: 'bar',
+                    values: [2500, 5000, 1000, 1500, 1800, 3200]
+                },
+            ],
+            tooltipOptions: {
+                formatTooltipX: d => (d + "Cash Flow"),
+                formatTooltipY: d => thousands_separators(d) + " BDT"
+            },
+            axisOptions: {
+                xAxisMode: "tick",
+                xIsSeries: true
+            },
+            barOptions: {
+                stacked: false,
+                spaceRatio: 0.5
+            },
+        }
+    },
+    computed:{
+        title(){
+            return 'Monthly Cash Flow'
+        }
+    }
+}
+
+
+function thousands_separators(num) {
+    let num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num_parts.join(".");
+}
+</script>
+
+<style>
+.frappe .title {
+    font-size: 14px;
+
+}
+</style>
---
+++ resources/js/pages/dashboard/widgets/MonthStatistics.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="level box shadow">
+            <span class="text-gray-700 flex flex-col align-items-center">
+                <small class="mb-0">Statistics</small>
+                <span class="text-uppercase tracking-wider">{{ current_month_name }}</span>
+            </span>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading text-green-700">Incomes</p>
+                <p class="title flex flex-row">
+                    <b-skeleton width="60px" height="10" :animated="false"/>
+                </p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading text-orange-600">Expenses</p>
+                <p class="title flex flex-row">
+                    <b-skeleton width="60px" height="10" :animated="false"/>
+                </p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading text-red-700">Bills to pay</p>
+                <p class="title flex flex-row">
+                    <b-skeleton width="60px" height="10" :animated="false"/>
+                </p>
+            </div>
+        </div>
+        <div class="level-item has-text-centered">
+            <div>
+                <p class="heading text-gray-800">Customer Due</p>
+                <p class="title flex flex-row text-red-600">
+                    <b-skeleton width="60px" height="10" :animated="false"/>
+                </p>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "MonthStatistics",
+    computed: {
+        current_month_name() {
+            return new Date().toLocaleString('default', {month: 'long'});
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
