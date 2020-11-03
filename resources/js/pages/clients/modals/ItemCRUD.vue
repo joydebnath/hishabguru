ce30b4e2f7777250bd55f1bae7dc2bf30ad7dafe
Joy Debnath <jd@genisys.com.au>
2020-11-03 20:30:39 +1100

    Client Create validation

--- resources/js/pages/clients/modals/ItemCRUD.vue
+++ resources/js/pages/clients/modals/ItemCRUD.vue
@@ -15,44 +15,85 @@
                 <button type="button" class="delete" @click="$emit('on-close')"></button>
             </header>
             <section class="modal-card-body">
-                <b-field label="Full Name">
-                    <b-input v-model="computed_item.name"></b-input>
+                <b-field
+                    label="Full Name"
+                    :type="computed_errors.name ? 'is-danger' :null"
+                    :message="computed_errors.name ? 'This field is required' : null"
+                >
+                    <b-input custom-class="pr-0" v-model="computed_item.name"></b-input>
                 </b-field>
                 <b-field grouped>
-                    <b-field label="Mobile">
-                        <b-input v-model="computed_item.mobile"></b-input>
+                    <b-field
+                        label="Mobile"
+                        :type="computed_errors.mobile ? 'is-danger' :null"
+                        :message="computed_errors.mobile ? 'This field is required' : null"
+                    >
+                        <b-input custom-class="pr-0" v-model="computed_item.mobile"></b-input>
                     </b-field>
                     <b-field label="Phone">
-                        <b-input v-model="computed_item.phone"></b-input>
+                        <b-input custom-class="pr-0" v-model="computed_item.phone"></b-input>
                     </b-field>
                 </b-field>
-                <b-field label="Emaill address">
-                    <b-input v-model="computed_item.email"></b-input>
+                <b-field label="Email address">
+                    <b-input custom-class="pr-0" v-model="computed_item.email"></b-input>
                 </b-field>
                 <p class="font-semibold mt-4 my-3 ">
                     <span class="mr-2">Contact address <b-tag>Home</b-tag></span>
                 </p>
-                <b-field>
-                    <b-input placeholder="Address line 1" v-model="computed_item.address_line_1"></b-input>
+                <b-field :type="computed_errors.address_line_1 ? 'is-danger' :null"
+                         :message="computed_errors.address_line_1 ? 'This field is required' : null"
+                >
+                    <b-input
+                        custom-class="pr-0"
+                        placeholder="Address line 1"
+                        v-model="computed_item.address_line_1"
+                    />
                 </b-field>
 
                 <b-field>
-                    <b-input placeholder="Address line 2" v-model="computed_item.address_line_2"></b-input>
+                    <b-input custom-class="pr-0"
+                             placeholder="Address line 2"
+                             v-model="computed_item.address_line_2"
+                    />
                 </b-field>
                 <b-field grouped>
-                    <b-field>
-                        <b-input placeholder="City/Town" v-model="computed_item.city"></b-input>
+                    <b-field
+                        :type="computed_errors.city ? 'is-danger' :null"
+                        :message="computed_errors.city ? 'This field is required' : null"
+                    >
+                        <b-input custom-class="pr-0" placeholder="City" v-model="computed_item.city" required></b-input>
                     </b-field>
-                    <b-field>
-                        <b-input placeholder="Postal/Zip Code" v-model="computed_item.postcode"></b-input>
+                    <b-field
+                        :type="computed_errors.postcode ? 'is-danger' :null"
+                        :message="computed_errors.postcode ? 'This field is required' : null"
+                    >
+                        <b-input
+                            custom-class="pr-0"
+                            placeholder="Postal"
+                            v-model="computed_item.postcode"
+                        />
                     </b-field>
                 </b-field>
                 <b-field grouped>
-                    <b-field>
-                        <b-input placeholder="Division" v-model="computed_item.state"></b-input>
+                    <b-field
+                        :type="computed_errors.state ? 'is-danger' :null"
+                        :message="computed_errors.state ? 'This field is required' : null"
+                    >
+                        <b-input
+                            custom-class="pr-0"
+                            placeholder="State/Division"
+                            v-model="computed_item.state"
+                        />
                     </b-field>
-                    <b-field>
-                        <b-input placeholder="Country" value="Bangladesh" v-model="computed_item.country"></b-input>
+                    <b-field
+                        :type="computed_errors.country ? 'is-danger' :null"
+                        :message="computed_errors.country ? 'This field is required' : null"
+                    >
+                        <b-input
+                            custom-class="pr-0"
+                            placeholder="Country"
+                            v-model="computed_item.country"
+                        />
                     </b-field>
                 </b-field>
                 <b-field label="Note">
@@ -91,19 +132,42 @@ export default {
                 state: '',
                 country: '',
                 note: '',
-            }
+            },
+            required_fields: {
+                name: true,
+                mobile: true,
+                address_line_1: true,
+                city: true,
+                postcode: true,
+                state: true,
+                country: true,
+            },
+            errors: {}
         }
     },
     methods: {
         loading_event(value) {
             this.$emit('on-loading', value)
         },
-        handleSave() {
-            if (this.$props.action_type == "edit") {
-                this.update();
-                return 0;
+        validate() {
+            let error_bag = {}
+
+            for (let value in this.computed_item) {
+                if (this.required_fields[value] && _.isEmpty(this.computed_item[value])) {
+                    error_bag[value] = true
+                }
             }
-            this.create()
+            this.errors = error_bag
+        },
+        handleSave() {
+            this.validate()
+            if (_.isEmpty(this.errors)) {
+                if (this.$props.action_type == "edit") {
+                    this.update();
+                    return 0;
+                }
+                this.create()
+            }
         },
         update() {
             this.loading_event(true);
@@ -114,19 +178,11 @@ export default {
                     this.$store.commit('clients/update', {client: data.data})
                     this.client = data.data
                     this.$emit('on-close')
-                    this.$buefy.notification.open({
-                        message: 'Client has been updated',
-                        type: 'is-success is-light',
-                        duration: 5000
-                    })
+                    this.onSuccess('Client has been updated')
                 })
                 .catch(err => {
                     this.loading_event(false);
-                    this.$buefy.notification.open({
-                        message: 'Client update failed',
-                        type: 'is-danger is-light',
-                        duration: 5000
-                    })
+                    this.onError('Client update failed')
                 })
         },
         create() {
@@ -137,30 +193,36 @@ export default {
                     this.loading_event(false);
                     this.client = {};
                     this.$emit('on-close');
-                    this.$buefy.notification.open({
-                        message: 'Client is created',
-                        type: 'is-success is-light',
-                        duration: 5000
-                    })
+                    this.onSuccess('Client is created')
                     if (this.total < this.per_page) {
                         this.$store.dispatch('clients/loadData', {page: 1})
                     }
                 })
                 .catch(err => {
                     this.loading_event(false);
-                    this.$buefy.notification.open({
-                        message: 'Whoops! Something went wrong...',
-                        type: 'is-danger is-light',
-                        duration: 5000
-                    })
+                    this.onError()
                 })
+        },
+        onSuccess(message) {
+            this.$buefy.notification.open({
+                message: message,
+                type: 'is-success is-light',
+                duration: 5000
+            })
+        },
+        onError(message = 'Whoops! Something went wrong...') {
+            this.$buefy.notification.open({
+                message: message,
+                type: 'is-danger is-light',
+                duration: 5000
+            })
         }
     },
     computed: {
         ...mapGetters({
             total: 'clients/getTotal',
             per_page: 'getPerPage',
-            current_tenant_id: 'tenancy/getCurrentTenant'
+            tenant_id: 'tenancy/getCurrentTenant'
         }),
         title() {
             return this.$props.action_type == "edit"
@@ -168,10 +230,13 @@ export default {
                 : "Add new Client";
         },
         computed_item() {
-            if (this.$props.item) {
+            if (!_.isEmpty(this.$props.item)) {
                 this.client = this.$props.item;
             }
-            return {...this.client, tenant_id: this.current_tenant_id}
+            return {...this.client, tenant_id: this.tenant_id}
+        },
+        computed_errors() {
+            return this.errors
         }
     }
 };
